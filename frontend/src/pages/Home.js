1552b560e1385b6d9e6fa30bee4ce94c8a9eef24
amritdubey1600 <115389809+amritdubey1600@users.noreply.github.com>
2023-10-02 08:17:41 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = ()=>{
 
     useEffect(()=>{
         const fetchWorkouts = async()=>{
-            const response =  await fetch('http://localhost:4000/api/workouts',{
+            const response =  await fetch('https://gym-buddy-2-0.onrender.com/api/workouts',{
                 headers:{
                     'Authorization':`Bearer ${user.token}`
                 }
